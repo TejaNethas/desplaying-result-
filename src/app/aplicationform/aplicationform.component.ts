5a76fcdd61211652fe625c31326871f02a8ec1f3
raj786 <goshikarajeshwari786@gmail.com>
2020-08-16 23:53:33 -0700

    taskupdate

---
+++ src/app/aplicationform/aplicationform.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aplicationform',
+  templateUrl: './aplicationform.component.html',
+  styleUrls: ['./aplicationform.component.scss']
+})
+export class AplicationformComponent implements OnInit {
+
+  constructor(
+   /*  public name:string,
+    public pasingYear:number,
+    public lastName:string,
+    public foradmitn?: string */
+  ) 
+  
+{ 
+ 
+}
+
+  ngOnInit(): void {
+  }
+
+}
